04510e7e6e03812e41fb59eb6418c814389b1b25
Eunho <76768140sa@gmail.com>
2024-09-13 20:33:17 +0900

    feat 신은호 #65

--- src/main/java/com/example/_team/controller/TravleUpdateController.java
+++ src/main/java/com/example/_team/controller/TravleUpdateController.java
@@ -1,5 +1,6 @@
 package com.example._team.controller;
 
+import com.example._team.web.dto.travelalbum.TravelAlbumRequestDTO;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -20,6 +21,9 @@ import com.example._team.web.dto.travelalbum.TravelAlbumResponseDTO.TravelAlbumR
 
 import lombok.RequiredArgsConstructor;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 @Controller
 @RequiredArgsConstructor
 @RequestMapping("/api/travel")
@@ -32,10 +36,23 @@ public class TravleUpdateController {
 	public String updateTravelBoard(@PathVariable Integer travelIdx, Model model) {
 		String email = SecurityContextHolder.getContext().getAuthentication().getName();
 	    Users user = userService.findByEmail(email);
-	    TravelAlbumDetailResponseDTO response = travelService.getTravelBoard(travelIdx, user);
-	    
+        TravelAlbumDetailResponseDTO response = travelService.getTravelBoard(travelIdx, user);
+        String dateRange = response.getDateRange();
+
+        // 날짜 포맷터 생성
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy.MM.dd");
+
+        // 문자열을 각각 시작일과 종료일로 분리
+        String[] dates = dateRange.split(" - ");
+
+        // 문자열을 LocalDate로 변환
+        LocalDate startDate = LocalDate.parse(dates[0], formatter);
+        LocalDate endDate = LocalDate.parse(dates[1], formatter);
+
         model.addAttribute("response", response);
         model.addAttribute("user", user);
+        model.addAttribute("startDate",startDate);
+        model.addAttribute("endDAte",endDate);
 	
 		return "view/travel/TravelEdit";
 	}
